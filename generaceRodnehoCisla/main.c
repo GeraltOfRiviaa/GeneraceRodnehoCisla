0306c8ef3294da22c6555e6b7e709337a4e73136
GeraltOfRiviaa <147801858+GeraltOfRiviaa@users.noreply.github.com>
2025-02-12 17:33:11 +0100

    Update main.c

--- generaceRodnehoCisla/main.c
+++ generaceRodnehoCisla/main.c
@@ -1,50 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
 
-int otazka(char *otazka) {
-    int odpoved;
-    printf("Zadej %s: ", otazka);
-    scanf("%d", &odpoved);
-    return odpoved;
-}
-int generaceCisla(int rok, int mesic, int den, int pohlavi, int posTroj) {
-    char id[5] = {00,00,00,000,0};
-    if (rok < 2000) {
-        id [0] = (rok - 1900) + '\0';
-    }
-    else {
-        id [0] = rok - 2000 + '\0';
-    }
-    if (pohlavi == 1) {
-        id[1] = mesic + 50 + '\0';
-    }
-    else if (mesic <=9) {
-        id[1] = '0' + den + '\0';
-    }
-    else {
-        id[1] = mesic + '\0';
-    }
-    if (den <= 9 ) {
-        id[2] = '0' + den + '\0';
-    }
-    else {
-        id[2] = den + '\0';
-    }
-    sprintf(id, "%02d%02d%02d/%03d", rok, mesic, den, posTroj);
-
-    return atoi(id);
+// Funkce pro kontrolu přestupného roku
+bool prestupnyRok(int rok) {
+    return (rok % 4 == 0 && rok % 100 != 0) || (rok % 400 == 0);
 }
 
-int main(void) {
-    int rok = otazka("rok");
-    int mesic = otazka("mesic");
-    int den= otazka("den");
-    int pohlavi = otazka("pohlavi (1 = zena, 0 = muz)");
-    int specialniIdentifikator = otazka("posledni trojcisli");
-    printf("%d",generaceCisla(rok, mesic, den, pohlavi, specialniIdentifikator));
+// Funkce pro získání počtu dní v měsíci
+int dnyVMesici(int mesic, int rok) {
+    int dny[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    if (mesic == 2 && prestupnyRok(rok)) {
+        return 29;
+    }
+    return dny[mesic - 1];
+}
 
 
 
+// Funkce pro generování rodného čísla
+void generateRodneCislo(int rok, int mesic, int den, char pohlavi, int unikatniCislo, char* rodneCislo[11]) {
+    
+    if (pohlavi == '1') {
+        mesic += 50;
+    }
+    
+    rok = rok % 100;
+    
+    // Sestavení prvních 9 číslic rodného čísla
+    sprintf(rodneCislo, "%02d%02d%02d%03d", rok, mesic, den, unikatniCislo);
+    
+    // Výpočet kontrolní číslice
+    long long cislo = atoll(rodneCislo);
+    int posledniCislo;
+    for(int i = 1; i < 10; i++) {
+        /*
+        if ((cislo * 10 + i) % 11 == 0) {
+            posledniCislo = i;
+            break;
+        }
+        */
+        if (cislo % 11 == i){
+            posledniCislo = i;
+            break;
+        }
+        else if (cislo % 11 == 10){
+            posledniCislo = 0;
+            break;
+        }
+    }
+    
+    // Přidání kontrolní číslice
+    sprintf(rodneCislo, "%02d%02d%02d%03d%d", rok, mesic, den, unikatniCislo, posledniCislo);
+}
+int otazka(const char *predmet, int intervalZ, int intervalK){
+    int Predmet;
+    int buffer = 0;
+    do
+    {
+        printf("Zadej %s v intervalu %d - %d: ", predmet, intervalZ, intervalK);
+    scanf("%d",&Predmet);
+    while (getchar() != '\n')
+    {
+        buffer++;
+    }
+    
+    if (Predmet < intervalZ || Predmet > intervalK){
+        printf("Jsi mimo interval!!!");
+    }
+    else if (buffer > 0){
+        printf("Zadal jsi pismeno!!!");
+    }
+    } while (buffer > 0 || Predmet < intervalZ || Predmet > intervalK);
+    
+    return Predmet;
+}
+
+int main() {
+    int rok, mesic, den, unikatniCisla, pohlavi;
+    char rodneCislo[11];
+    
+        rok = otazka("rok", 1954,2025);
+        mesic = otazka("mesic", 1,12);
+        den = otazka("den", 1,dnyVMesici(mesic, rok));
+        pohlavi = otazka("pohlavi", 0,1);
+        unikatniCisla = otazka("unikatni cislo", 0,999);
+    
+    // Generování rodného čísla
+    generateRodneCislo(rok, mesic, den, pohlavi, unikatniCisla, rodneCislo);
+    
+    // Výpis výsledku
+    printf("\nVygenerovane validni rodne cislo: %s\n", rodneCislo);
+    
     return 0;
 }
